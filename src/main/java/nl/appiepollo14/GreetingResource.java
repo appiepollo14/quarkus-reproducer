78c28e3d75140135c29882c9ce232a439e1a9944
asjervanasten <asjer94@live.com>
2024-05-14 06:43:56 +0200

    otel reproducer

--- src/main/java/nl/appiepollo14/GreetingResource.java
+++ src/main/java/nl/appiepollo14/GreetingResource.java
@@ -1,32 +1,18 @@
 package nl.appiepollo14;
 
-import io.quarkus.vertx.ConsumeEvent;
-import io.smallrye.common.annotation.Blocking;
-import io.smallrye.common.annotation.NonBlocking;
-import io.vertx.core.eventbus.EventBus;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import org.jboss.logging.Logger;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @Inject
-    EventBus bus;
-
-    @Inject
-    ProcessingService processingService;
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     @GET
-    @NonBlocking
     public String start() {
-        bus.send("topic", "dummyBody");
+        LOG.info("Called hello GET endpoint");
+        LOG.error("ERROR");
         return "Hello from Quarkus REST";
-    }
-
-    @Blocking
-    @ConsumeEvent("topic")
-    public void processing(String body) {
-        processingService.start();
     }
 }
--- src/test/java/nl/appiepollo14/GreetingResourceTest.java
+++ src/test/java/nl/appiepollo14/GreetingResourceTest.java
@@ -1,19 +1,13 @@
 package nl.appiepollo14;
 
-import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
-@QuarkusTestResource(ClientMock.class)
 class GreetingResourceTest {
-
-    @Inject
-    GreetingResource sut;
 
     @Test
     public void testProcessing() {
